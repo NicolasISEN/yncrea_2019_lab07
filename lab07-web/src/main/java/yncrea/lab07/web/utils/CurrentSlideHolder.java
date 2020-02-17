31fecd2383a33f0e5b2768463bece910b530852f
Clara <cb.clara1309@gmail.com>
2020-02-17 09:19:01 +0100

    add currentSlideHolder class

---
+++ lab07-web/src/main/java/yncrea/lab07/web/utils/CurrentSlideHolder.java
@@ -0,0 +1,28 @@
+package yncrea.lab07.web.utils;
+
+import yncrea.lab07.web.dto.Slide;
+
+public class CurrentSlideHolder {
+    private Slide slide;
+    private static CurrentSlideHolder currentSlideHolder;
+
+    public Slide getSlide() {
+        return slide;
+    }
+
+    public void setSlide(Slide slide) {
+        this.slide = slide;
+    }
+
+    private CurrentSlideHolder() {
+    }
+
+    public static CurrentSlideHolder getInstance() {
+        if (currentSlideHolder==null) {
+            return new CurrentSlideHolder();
+        }
+        return currentSlideHolder;
+    }
+
+
+}
